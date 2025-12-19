a5fac840c02ae43f1005ed0d3299709d51947e03
Hayden Phan <haydenphancs@gmail.com>
2025-12-18 17:51:52 -0700

    f

---
+++ MarketPulse/MarketPulse/MarketPulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketPulseApp.swift
+//  MarketPulse
+//
+//  Created by Hai Phan on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MarketPulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

b96b0e7b3906f4cb028225732ff59b6e5b3006a1
haydenphancs <haydenphancs@email.com>
2025-12-23 11:59:21 -0700

    f

---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by Hai Phan on 12/23/25.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
